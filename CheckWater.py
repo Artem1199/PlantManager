1b15cb82c546157f1c62622a09bf9e8c704f7733
Artem1199 <artkulak@gmail.com>
2021-11-26 10:00:00 -0800

    Updating comments

--- CheckWater.py
+++ CheckWater.py
@@ -1,7 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
 from ReadWriteConfig import *
 import Adafruit_ADS1x15
+
+now = datetime.datetime.now()
+
+print("Starting CheckWater.py", str(now))
 
 adc = Adafruit_ADS1x15.ADS1015() # Pick Sensors
 
@@ -10,15 +15,13 @@ SS_COUNT = 0
 with open("PlantMgr.xml", "r") as f:
     content = f.read()
     y = BeautifulSoup(content, features = "lxml")
-    #print(y)
-    print(y.find("gain"))
     GAIN = int(y.find("gain").text)
     SS_COUNT = int(y.find("ss_count").text)
 
 mSS_Array = np.full(5, SoilSensors(0,0.0, 0.0,0, 0.0))
 SS_ARRAY = ReadWriteConfig(0, mSS_Array, SS_COUNT)
 
-print(SS_ARRAY)
+#print("SS_ARRAY Value: ", SS_ARRAY)
 
 PWM0Pin = 18
 PWM1Pin = 12
@@ -41,26 +44,22 @@ pwm_array = [pwm0, pwm1]
 GPIO.setup(LED0Pin, GPIO.OUT)
 GPIO.output(LED0Pin, GPIO.HIGH)
 
-
-
-
-
 for item in SS_ARRAY:
-	print("index: ", item.index)
-	print("Setpoint: ", item.setpoint)
-	print("Cal Value: ", item.a)
-	print("b value: ", item.b)
-	#SensorVal = adc.read_adc(item.index, gain = GAIN)
+	print("Sensor Index: ", item.index)
+	print("Sensor #", item.index," config setpoint:", item.setpoint)
+	print("Sensor #", item.index," config Cal A value:", round(item.a,2))
+	print("Sensor #", item.index," config Cal B value:", round(item.b,2))
+
 	SensorVal = 0
 	for _ in range(5):
 		SensorVal += adc.read_adc(item.index, gain = GAIN)
 		time.sleep(0.1)
 	SensorVal = SensorVal / 5 
-	print("Sensor reading: ", SensorVal)
+	print("Sensor #", item.index," sensor reading:", SensorVal)
 	a = item.a
 	b = item.b
 	setpoint = item.setpoint
-	print("Sensor * cal + b: ", SensorVal * a + b)
+	print("Sensor #", item.index," recalibrated (0-100):", round(SensorVal * a + b,2), "/", item.setpoint)
 
 	# run at low power to indicate running
 	pwm_array[item.index].start(10)
@@ -69,8 +68,8 @@ for item in SS_ARRAY:
 
 	overflowCounter = 0
 	while((SensorVal * a + b) > setpoint):
-		if overflowCounter > 15:
-			print("Reached overflow counter")
+		if overflowCounter > 40:
+			print("Error: Reached overflow counter.")
 			break
 		# check for wire open on sensor
 		if SensorVal < 1200:
@@ -88,9 +87,9 @@ for item in SS_ARRAY:
 				time.sleep(0.1)
 
 			SensorVal = SensorVal / 5 
-			print("Sensor val measurement loop: ", SensorVal * a + b)
+			print("Counter:", overflowCounter, "Sensor #", item.index," recalibrated (0-100):", SensorVal * a + b)
 		else:
-			print("Check sensor connection.")
+			print("Error: Check sensor connection.")
 			break
 
 	pwm_array[item.index].stop(0)
@@ -98,3 +97,7 @@ for item in SS_ARRAY:
 # hold LED on after completion
 time.sleep(10)
 GPIO.output(LED0Pin, GPIO.LOW)
+GPIO.cleanup()
+print("Ending CheckWater.py", str(now))
+
+
